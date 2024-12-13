c58b772ff495860850969393c8863f8d45bf1177
Stefan Rifel <s.rifel23i@gmail.com>
2024-12-13 22:14:33 +0100

    Add drehendes model

--- src/Scene.h
+++ src/Scene.h
@@ -165,16 +165,21 @@ public:
 
         /*
         UniformBufferObject ubo{};
-        ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        ubo.model = 
         ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         ubo.proj = glm::perspective(glm::radians(45.0f), SwapChain::getSwapChainExtent().width / (float) SwapChain::getSwapChainExtent().height, 0.1f, 10.0f);
         ubo.proj[1][1] *= -1;
         */
         camera.look();
 
+        static auto startTime = std::chrono::high_resolution_clock::now();
+
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+
+
         UniformBufferObject ubo{};
-        glm::mat4 model = glm::mat4(1);
-        ubo.model = model; //glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         ubo.view = camera.getView();
         ubo.proj = camera.getPerspective();
 
